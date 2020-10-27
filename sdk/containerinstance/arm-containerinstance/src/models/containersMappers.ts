5dd6e12b22e34cb4cfbca22eb5e9951663ab39ec
SDKAuto <sdkautomation@microsoft.com>
2020-10-27 23:56:42 +0000

    CodeGen https://github.com/test-repo-billy/azure-rest-api-specs/pull/1911 Merge b5d93db460e856c16fb275e3bb8231269ea05a9f into e21b960c504994bb94e16f96bf72d6da6ac06d25

--- sdk/containerinstance/arm-containerinstance/src/models/containersMappers.ts
+++ sdk/containerinstance/arm-containerinstance/src/models/containersMappers.ts
@@ -8,7 +8,8 @@
 
 export {
   CloudError,
-  Operation,
-  OperationDisplay,
-  OperationListResult
+  ContainerExecRequest,
+  ContainerExecRequestTerminalSize,
+  ContainerExecResponse,
+  Logs
 } from "../models/mappers";
