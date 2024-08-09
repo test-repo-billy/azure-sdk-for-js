36b5300c8fb3f247a9ac21073bb81b788ea7bb2a
Wanpeng Li <wanl@microsoft.com>
2024-08-10 03:37:39 +0800

    Update automation_init.sh

--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -5,4 +5,4 @@ export NVM_DIR="$HOME/.nvm"
 nvm install v18.15.0 
 nvm alias default node
 npm install -g @microsoft/rush@5.92.0
-npm install -g js-sdk-release-tools@2.7.19-beta
+npm install -g js-sdk-release-tools@2.7.20-beta
