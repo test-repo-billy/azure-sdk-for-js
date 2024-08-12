e62d80bd5c6b98d48c4e3183b90548ecad3a265a
wanl <wanl@microsoft.com>
2024-08-12 13:19:04 +0800

    Fix

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -8,17 +8,20 @@ file_path="../azure-rest-api-specs/branch.txt"
 if [ -f "$file_path" ]; then
     # Load the content of the file into a variable
     branch=$(cat "$file_path")
-    echo "Branch file content loaded into variable."
+    echo "switch to branch $branch"
 else
     echo "Branch file does not exist."
 fi
-branch=$($file_path)
-echo "switch to branch $branch"
+
 
 cd ../..
 git clone https://github.com/Azure/azure-sdk-tools/
 cd azure-sdk-tools
-git checkout -b test $branch
-cd ../azure-sdk-for-js
-pnpm install
+if [ -f "$file_path" ]; then
+    git checkout -b test $branch
+fi
+git status
+
+cd ../azure-sdk-for-js_tmp
+npm install
 tsx ../azure-sdk-tools/tools/js-sdk-release-tools/src/autoGenerateInPipeline.ts --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.12 --typespecEmitter=@azure-tools/typespec-ts
