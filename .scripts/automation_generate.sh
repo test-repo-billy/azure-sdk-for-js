c40fc5a0a8fea02080e397c954b31588adb1cd82
wanl <wanl@microsoft.com>
2024-08-12 11:40:50 +0800

    Update path

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -3,7 +3,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm use default
 
-branch=$(../../azure/azure-rest-api-specs/branch.txt)
+branch=$(../azure-rest-api-specs/branch.txt)
 echo "switch to branch $branch"
 
 cd ../..
@@ -12,5 +12,4 @@ cd azure-sdk-tools
 git checkout -b test $branch
 cd ../azure-sdk-for-js
 pnpm install
-cd .scripts
-tsx ../../azure-sdk-tools/tools/js-sdk-release-tools/src/autoGenerateInPipeline.ts --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.12 --typespecEmitter=@azure-tools/typespec-ts
+tsx ../azure-sdk-tools/tools/js-sdk-release-tools/src/autoGenerateInPipeline.ts --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.12 --typespecEmitter=@azure-tools/typespec-ts
