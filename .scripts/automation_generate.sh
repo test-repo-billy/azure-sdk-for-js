0b8ab47fbdd7159e46aaaa7dce5b16ec1c285b01
wanl <wanl@microsoft.com>
2024-08-12 11:04:03 +0800

    Support multi branch test

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -2,4 +2,15 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm use default
-code-gen-pipeline --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.12 --typespecEmitter=@azure-tools/typespec-ts
+
+branch=$(../../azure/azure-rest-api-specs/branch.txt)
+echo "switch to branch $branch"
+
+cd ../..
+git clone https://github.com/Azure/azure-sdk-tools/
+cd azure-sdk-tools
+git checkout -b test $branch
+cd ../azure-sdk-for-js
+pnpm install
+cd .scripts
+tsx ../../azure-sdk-tools/tools/js-sdk-release-tools/src/autoGenerateInPipeline.ts --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.12 --typespecEmitter=@azure-tools/typespec-ts
--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -5,4 +5,5 @@ export NVM_DIR="$HOME/.nvm"
 nvm install v18.15.0 
 nvm alias default node
 npm install -g @microsoft/rush@5.92.0
-npm install -g js-sdk-release-tools@2.7.21-beta
+npm install -g tsx
+
