f0f4b4f361a54df205339fe5b1feb0745f95b129
wanl <wanl@microsoft.com>
2024-08-12 23:03:17 +0800

    a

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -34,5 +34,5 @@ pnpm install
 pnpm run build
 cd ../../..
 
-cd azure-sdk-for-js_tmp
+cd azure-sdk-for-js
 node ../azure-sdk-tools/tools/js-sdk-release-tools/dist/autoGenerateInPipeline.js --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@^6.0.12 --typespecEmitter=@azure-tools/typespec-ts
